e40b86f22cc28b715933f0d5bd76b08f39634d2d
Shanur <shanurislam@gmail.com>
2024-02-27 01:48:53 +0000

    Lesson 25

--- src/App.js
+++ src/App.js
@@ -9,40 +9,40 @@ import './App.css';
 // import EventBind from './components/EventBind';
 // import ParentComponent from './components/ParentComponent';
 // import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
+// import Stylesheet from './components/stylesheets';
+// import Inline from './components/Inline';
+// import './appStyles.css'
+// import styles from './appStyles.module.css'
+// import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
 
 function App() {
   return (
     <div className="App">
-        <NameList />
+        <LifecycleA />
+        {/* <Form/> */}
+        {/* <h1 className='error'>Error</h1> */}
+        {/* <h1 className={styles.success}>Success</h1> */}
+        {/* <Inline /> */}
+        {/* <Stylesheet primary={true}/> */}
+        {/* <NameList /> */}
         {/* <UserGreeting /> */}
-
         {/* <ParentComponent /> */}
-        
         {/* <EventBind /> */}
-        
         {/* <ClassClick /> */}
-        
         {/* <FunctionClick /> */}
-        
         {/* <Counter addValue='2'/> */}
-        
         {/* <Message /> */}
-
         {/* <Greet name="Bruce" heroName="Batman">
             <p>This is a child prop</p>
         </Greet> */}
-
         {/* <Greet name="Clark"heroName="Superman">
             <button>Action</button>
         </Greet> */}
-
         {/* <Greet name="Estelle" heroName="Child of the Fullmoon"></Greet> */}
-        
         {/* <Welcome name="Bruce" heroName="Batman"/> */}
         {/* <Welcome name="Clark"heroName="Superman"/> */}
         {/* <Welcome name="Estelle" heroName="Child of the Fullmoon"/> */}
-        
         {/* <Hello /> */}
     </div>
   );
