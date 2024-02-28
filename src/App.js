764ecd535dfa05ab9c324bb5c61b5dc5c5ed3d4c
Shanur <shanurislam@gmail.com>
2024-02-28 01:05:08 +0000

    set-up react app manually
    - Deleted and started again due to the dependency bugs that are present when `npx create-react-app`
    - Used the guide from https://medium.com/@claude.ando/setting-up-a-react-project-without-create-react-app-6ff7fea9ca51 to setup my react app
    - This can be treated as an initial commit now
    - Will add all of the additional lesson files so far

--- src/App.js
+++ src/App.js
@@ -1,51 +1,7 @@
-import './App.css';
-// import Greet from './components/Greet';
-// import Welcome from './components/Welcome';
-// import Hello from './components/Hello';
-// import Message from './components/Message';
-// import Counter from './components/Counter';
-// import FunctionClick from './components/FunctionClick';
-// import ClassClick from './components/ClassClick';
-// import EventBind from './components/EventBind';
-// import ParentComponent from './components/ParentComponent';
-// import UserGreeting from './components/UserGreeting';
-// import Stylesheet from './components/stylesheets';
-// import Inline from './components/Inline';
-// import './appStyles.css'
-// import styles from './appStyles.module.css'
-// import Form from './components/Form';
-import LifecycleA from './components/LifecycleA';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-        <LifecycleA />
-        {/* <Form/> */}
-        {/* <h1 className='error'>Error</h1> */}
-        {/* <h1 className={styles.success}>Success</h1> */}
-        {/* <Inline /> */}
-        {/* <Stylesheet primary={true}/> */}
-        {/* <NameList /> */}
-        {/* <UserGreeting /> */}
-        {/* <ParentComponent /> */}
-        {/* <EventBind /> */}
-        {/* <ClassClick /> */}
-        {/* <FunctionClick /> */}
-        {/* <Counter addValue='2'/> */}
-        {/* <Message /> */}
-        {/* <Greet name="Bruce" heroName="Batman">
-            <p>This is a child prop</p>
-        </Greet> */}
-        {/* <Greet name="Clark"heroName="Superman">
-            <button>Action</button>
-        </Greet> */}
-        {/* <Greet name="Estelle" heroName="Child of the Fullmoon"></Greet> */}
-        {/* <Welcome name="Bruce" heroName="Batman"/> */}
-        {/* <Welcome name="Clark"heroName="Superman"/> */}
-        {/* <Welcome name="Estelle" heroName="Child of the Fullmoon"/> */}
-        {/* <Hello /> */}
-    </div>
-  );
-}
+const App = () => {
+    return <div>Hello, world!</div>;
+};
 
 export default App;
